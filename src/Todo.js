59751364546bf34cb92ba15f23227174e2b058a6
ayushraj-k <ayushkandari09@gmail.com>
2020-06-03 14:37:48 +0530

    first

---
+++ src/Todo.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+
+class Todo extends Component {
+
+    constructor(props){
+        super(props)
+            this.state={
+                item:"", 
+                todoItems:[]
+        }
+        this.onChangeHandler = this.onChangeHandler.bind(this);
+        this.addData = this.addData.bind(this);
+        this.delete = this.delete.bind(this);
+    }
+
+    onChangeHandler(event){
+        var inputVal = event.target.value; 
+       this.setState({
+           item:inputVal
+       })
+        
+    }
+
+    addData() {
+        var inputVal = this.state.item; 
+        var itemInstance = this.state.todoItems;
+        itemInstance.push(inputVal);
+        this.setState({
+           todoItems:itemInstance, 
+           item:''
+        })
+    }
+
+    delete(event){
+        var id = event.target.id; 
+        var itemInstance = this.state.todoItems;
+        itemInstance.splice(id,1);
+        this.setState({
+            item:itemInstance
+           
+        })
+
+    }
+    render() {
+        var itemList = this.state.todoItems.map((e,i)=>
+            <li key={i}>{e} <span onClick={this.delete} id={i}>x</span></li>
+        )
+        return(
+            <div>
+                <div className="header">React js Todoapp</div>
+                <div className="body">
+                    <ul>
+                        {itemList}
+                    </ul>
+                </div>
+                <div className="footer">
+                    <input type="text" value={this.state.item} onChange={this.onChangeHandler}/>
+                    <button onClick={this.addData}>Add</button>
+
+                </div>
+
+                
+            </div>
+        )
+    }
+}
+
+export default Todo;
